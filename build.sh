b2176880675259f2c810e15c01fcd1169929552c
ssda <abdh357@gmail.com>
2023-09-25 12:00:53 +0000

    impl opengl tri with red colorw

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
-
-clang -Wall -Wextra -o build/main main.c -lglfw -lm -lXrandr -lXi -lXxf86vm -lpthread -lX11 -ldl -lXinerama -lXcursor
+rm -rf build
+mkdir build
+clang -Wall -Wextra -o build/main main.c -lglfw -lm -lXrandr -lXi -lXxf86vm -lpthread -lX11 -ldl -lXinerama -lXcursor -lGL -lglut -lGLEW
 echo done
-./main
+./build/main
--- main.c
+++ main.c
@@ -1,10 +1,9 @@
-#include <GL/gl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-
 #define GLFW_DLL
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #define IMG_WIDTH 900
@@ -26,9 +25,58 @@ static void error_callback(int error, const char* description)
   fprintf(stderr, "Error: %s\n", description);
 }
 
+static unsigned int CompileShader(unsigned int type, const char* source) {
+
+  unsigned int id = glCreateShader(type);
+  glShaderSource(id, 1, &source, NULL);
+  glCompileShader(id);
+
+  int result;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+  if (result == GL_FALSE) {
+    int length;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    char* message = (char*) alloca(length * sizeof(char));
+
+    glGetShaderInfoLog(id, length, &length, message);
+    printf("Fail to compile shader");
+    if (type == GL_VERTEX_SHADER) {
+      printf("vertex \n");
+    } else if (type == GL_FRAGMENT_SHADER) {
+      printf("fragment \n");
+    } else {
+      printf("Unkown error %d", type);
+    } 
+    glDeleteShader(id);
+    return EXIT_SUCCESS;
+  
+  }
+
+  
+  return id;
+}
+
+static unsigned int CreateShader(const char* vertexShader, const char* fragmentShader) {
+  unsigned int program = glCreateProgram();
+  unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return program;
+
+}
+
 int main()
 {
-  printf("ssda new run");
+  printf("\n");
 
   glfwSetErrorCallback(error_callback);
 
@@ -49,8 +97,55 @@ int main()
 
   glfwMakeContextCurrent(window);
 
+  if (glewInit()) {
+    printf("failed to init glew");
+  }
+
+  float positions[6] = {
+    -.5f, -.5f,
+    .0f, .5f,
+    .5f, -.5f,
+  };
+
+  unsigned int buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+  char* vertexShader = 
+    "#version 330 core\n"
+    "\n"
+    "layout(location = 0) in vec4 position;"
+    "\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = position;\n"
+    "\n}"
+  ;
+
+  char* fragmentShader = 
+    "#version 330 core\n"
+    "\n"
+    "layout(location = 0) out vec4 color;\n"
+    "\n"
+    "void main()\n"
+    "{\n"
+    " color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "\n}"
+  ;
+
+  unsigned int shader = CreateShader(vertexShader, fragmentShader);
+  glUseProgram(shader);
+
+
   while (!glfwWindowShouldClose(window)) {
-    // glClear(GL_COLOR_BUFFER_BIT);
+     glClear(GL_COLOR_BUFFER_BIT);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -58,5 +153,6 @@ int main()
 
   glfwTerminate();
   
+  printf("\n");
   return EXIT_SUCCESS;
 }
