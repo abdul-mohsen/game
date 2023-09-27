b4926942c9f88037789cfa25e7dc89bb88139c9b
ssda <abdh357@gmail.com>
2023-09-27 04:22:18 +0000

    Added Index Buffers

--- main.c
+++ main.c
@@ -170,19 +170,30 @@ int main()
     printf("failed to init glew\n");
   }
 
-  float positions[6] = {
-    -.5f, -.5f,
-    .0f, .5f,
+  float positions[] = {
     .5f, -.5f,
+    .5f, .5f,
+    -.5f, -.5f,
+    -.5f, .5f,
+  };
+
+  unsigned int indices[] = {
+    0,1,2, 
+    1,2,3
   };
 
   unsigned int buffer;
   glGenBuffers(1, &buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+  unsigned int ibo;
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
   struct Shader *mShader = parseShader("res/shaders/Basic.shader");
   unsigned int shader = CreateShader(mShader->vertex, mShader->fragment);
@@ -190,9 +201,9 @@ int main()
 
 
   while (!glfwWindowShouldClose(window)) {
-     glClear(GL_COLOR_BUFFER_BIT);
-    
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
